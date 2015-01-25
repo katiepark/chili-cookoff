d7ca95a7f3e441d8c92fd9f39c56ea759083bfe5
katiepark <katie.e.park@gmail.com>
2015-01-24 23:11:19 -0500

    Okay i can post to a db now

--- chili/routes/index.js
+++ chili/routes/index.js
@@ -25,4 +25,32 @@ router.get('/newchili', function(req, res) {
     res.render('newchili', { title: 'Add new chili' });
 });
 
+/* POST to New Chili Service */
+router.post('/addchili', function(req, res) {
+    var db = req.db;
+
+    // Get form values from name attributes
+    var chili_name = req.body.chiliname,
+        chili_creator = req.body.chili_creator;
+
+    // Set collection
+    var collection = db.get('dishcollection');
+
+    // Submit to DB
+    collection.insert({
+        'dishname': chili_name,
+        'creator': chili_creator
+    }, function(err, doc) {
+        if (err) {
+            // Return an error if failure
+            res.send('There was a problem adding information to the database.');
+        } else {
+            // Set header so address bar doesn't still say /addchili
+            res.location('dishlist');
+            // Forward to success page
+            res.redirect('dishlist');
+        }
+    });
+});
+
 module.exports = router;
