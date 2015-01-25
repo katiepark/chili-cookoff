e4cfeb0a0f4a37feec15d5db15652256886452d9
katiepark <katie.e.park@gmail.com>
2015-01-24 22:53:55 -0500

    wow, a list of fake dish names

--- chili/routes/index.js
+++ chili/routes/index.js
@@ -10,4 +10,14 @@ router.get('/helloworld', function(req, res, next) {
     res.render('helloworld', { title: 'Hello, world!' });
 });
 
+router.get('/dishlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('dishcollection');
+    collection.find({}, {}, function(e,docs) {
+        res.render('dishlist', {
+            'dishlist': docs
+        });
+    });
+});
+
 module.exports = router;
