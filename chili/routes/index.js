3e53f3b373c339f5f3e85de2ba673b5cb260ac22
katiepark <katie.e.park@gmail.com>
2015-01-25 01:07:10 -0500

    Chili submission page, looking like crap

--- chili/routes/index.js
+++ chili/routes/index.js
@@ -6,19 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The PostGraphics chili cook-off' });
 });
 
-router.get('/dishlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('dishcollection');
-    collection.find({}, {}, function(e,docs) {
-        res.render('dishlist', {
-            'dishlist': docs
-        });
-    });
-});
-
 /* GET to New Chili Service */
 router.get('/newchili', function(req, res) {
-    res.render('newchili', { title: 'Add new chili' });
+    var db = req.db,
+        collection = db.get('chili_info');
+
+    collection.find({}, {}, function(e, docs) {
+        res.render('newchili', {
+            title: 'Add new chili',
+            dishlist: docs
+        });
+    });
 });
 
 /* POST to New Chili Service */
@@ -26,25 +24,24 @@ router.post('/addchili', function(req, res) {
     var db = req.db;
 
     // Get form values from name attributes
-    var chili_name = req.body.chiliname,
-        chili_creator = req.body.chili_creator;
+    var dishname = req.body.chiliname,
+        creator = req.body.chilicreator,
+        vegetarian = req.body.chiliveg;
 
     // Set collection
-    var collection = db.get('dishcollection');
+    var collection = db.get('chili_info');
 
     // Submit to DB
     collection.insert({
-        'dishname': chili_name,
-        'creator': chili_creator
+        'dishname': dishname,
+        'creator': creator,
+        'vegetarian': vegetarian || false
     }, function(err, doc) {
         if (err) {
             // Return an error if failure
             res.send('There was a problem adding information to the database.');
         } else {
-            // Set header so address bar doesn't still say /addchili
-            res.location('dishlist');
-            // Forward to success page
-            res.redirect('dishlist');
+            res.redirect('newchili');
         }
     });
 });
